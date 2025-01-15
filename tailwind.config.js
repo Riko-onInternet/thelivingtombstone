7a8aef7eed94545081ec4cd54426895a6d6efee4
Riko-onInternet <andreadelaurentis15@gmail.com>
2025-01-15 18:20:21 +0100

    sistemaggio di tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+import { nextui } from "@nextui-org/react";
+import daisyui from "daisyui";
+import forms from "@tailwindcss/forms";
+
 export default {
   content: [
     "./index.html",
@@ -8,18 +12,14 @@ export default {
   theme: {
     extend: {},
     screens: {
-      "xs": '405px',
-      "sm": '576px',
-      "md": '768px',
-      "lg": '992px',
-      "xl": '1200px',
-      "2xl": '1400px',
+      xs: "405px",
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      "2xl": "1400px",
     },
   },
   darkMode: "class",
-  plugins: [
-    require("@nextui-org/react"), 
-    require('daisyui'),
-    require('@tailwindcss/forms')
-  ],
-}
+  plugins: [nextui(), daisyui, forms],
+};
